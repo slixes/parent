d2bba34514137388b32479aa5c4a8d5df0603ddb
Fady Matar <fmatar@slixes.io>
2017-11-25 11:43:38 -0800

    Fixes to deployment script

--- .conf/deploy.sh
+++ .conf/deploy.sh
@@ -1,9 +1,13 @@
 #!bin/bash
 
 echo Repo: $TRAVIS_REPO_SLUG Pull Request: $TRAVIS_PULL_REQUEST Branch: $TRAVIS_BRANCH
-if [ "$TRAVIS_REPO_SLUG" == "slixes/parent" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+
+if [ "$TRAVIS_REPO_SLUG" = "slixes/parent" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_BRANCH" = "master" ]
+
+then
   export GIT_COUNT=`git rev-list HEAD --count`
   echo "Starting publish to Sonatype...  $GIT_COUNT"
+
     mvn deploy
   RETVAL=$?
 
@@ -13,4 +17,5 @@ if [ "$TRAVIS_REPO_SLUG" == "slixes/parent" ] && [ "$TRAVIS_PULL_REQUEST" == "fa
     echo 'Publish failed.'
     return 1
   fi
+
 fi
